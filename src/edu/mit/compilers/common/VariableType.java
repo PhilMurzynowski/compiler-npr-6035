df227e25c4e067760a91da3182c366cbb01c08c2
Robert Durfee <rbd@csail.mit.edu>
2021-03-14 09:25:47 -0500

    added toMethodType for VariableType

--- src/edu/mit/compilers/common/VariableType.java
+++ src/edu/mit/compilers/common/VariableType.java
@@ -1,6 +1,23 @@
 package edu.mit.compilers.common;
 
 public enum VariableType {
-  INTEGER,
-  BOOLEAN,
+  INTEGER {
+
+    @Override
+    public MethodType toMethodType() {
+      return MethodType.INTEGER;
+    }
+
+  },
+
+  BOOLEAN {
+
+    @Override
+    public MethodType toMethodType() {
+      return MethodType.BOOLEAN;
+    }
+
+  };
+
+  public abstract MethodType toMethodType();
 }
