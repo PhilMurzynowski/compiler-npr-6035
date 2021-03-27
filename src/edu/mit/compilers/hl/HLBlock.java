6bbd3f074256c2d8ebb3a11994829048b7986463
PhilMurzynowski <filipjasper@gmail.com>
2021-03-27 12:14:26 -0400

    adding constructors

--- src/edu/mit/compilers/hl/HLBlock.java
+++ src/edu/mit/compilers/hl/HLBlock.java
@@ -9,7 +9,13 @@ class HLBlock implements HLNode {
   private final List<HLArrayArrayFieldDeclaration> arrayFieldDeclarations;
   private final List<HLStatement> statements;
 
-  public HLBlock() {
+  // NOTE: may want a builder
+  public HLBlock( 
+    private final List<HLArgumentDeclaration> argumentDeclarations,
+    private final List<HLLocalScalarFieldDeclaration> scalarFieldDeclarations,
+    private final List<HLArrayArrayFieldDeclaration> arrayFieldDeclarations,
+    private final List<HLStatement> statements)
+  {
     throw new RuntimeException("not implemented");
   }
 
