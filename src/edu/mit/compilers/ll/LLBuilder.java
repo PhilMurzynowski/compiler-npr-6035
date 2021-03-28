9872b9cac4b4f469049e0bf809a249bf95f1a168
Robert Durfee <rbd@csail.mit.edu>
2021-03-27 19:38:53 -0500

    added LLMethodDeclaration to LLBuilder methods

--- src/edu/mit/compilers/ll/LLBuilder.java
+++ src/edu/mit/compilers/ll/LLBuilder.java
@@ -62,107 +62,107 @@ public class LLBuilder {
   }
 
   // TODO: Noah
-  public static LLControlFlowGraph buildStatement(HLStatement statement, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
+  public static LLControlFlowGraph buildStatement(HLStatement statement, LLMethodDeclaration methodDeclaration, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Robert
-  public static LLControlFlowGraph buildStoreStatement(HLStoreStatement storeStatement) {
+  public static LLControlFlowGraph buildStoreStatement(HLStoreStatement storeStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Phil
-  public static LLControlFlowGraph buildStoreScalarStatement(HLStoreScalarStatement scalarStoreStatement) {
+  public static LLControlFlowGraph buildStoreScalarStatement(HLStoreScalarStatement scalarStoreStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildStoreArrayStatement(HLStoreArrayStatement arrayStoreStatement) {
+  public static LLControlFlowGraph buildStoreArrayStatement(HLStoreArrayStatement arrayStoreStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildCallStatement(HLCallStatement callStatement) {
+  public static LLControlFlowGraph buildCallStatement(HLCallStatement callStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildIfStatement(HLIfStatement ifStatement, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
+  public static LLControlFlowGraph buildIfStatement(HLIfStatement ifStatement, LLMethodDeclaration methodDeclaration, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildForStatement(HLForStatement forStatement) {
+  public static LLControlFlowGraph buildForStatement(HLForStatement forStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildWhileStatement(HLWhileStatement whileStatement) {
+  public static LLControlFlowGraph buildWhileStatement(HLWhileStatement whileStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildReturnStatement(HLReturnStatement returnStatement) {
+  public static LLControlFlowGraph buildReturnStatement(HLReturnStatement returnStatement, LLMethodDeclaration methodDeclaration) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildBreakStatement(HLBreakStatement breakStatement, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
+  public static LLControlFlowGraph buildBreakStatement(HLBreakStatement breakStatement, LLMethodDeclaration methodDeclaration, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildContinueStatement(HLContinueStatement continueStatement, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
+  public static LLControlFlowGraph buildContinueStatement(HLContinueStatement continueStatement, LLMethodDeclaration methodDeclaration, Optional<LLBasicBlock> breakTarget, Optional<LLBasicBlock> continueTarget) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildArgument(HLArgument argument, LLDeclaration result) {
+  public static LLControlFlowGraph buildArgument(HLArgument argument, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Noah
-  public static LLControlFlowGraph buildExpression(HLExpression expression, LLDeclaration result) {
+  public static LLControlFlowGraph buildExpression(HLExpression expression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Robert
-  public static LLControlFlowGraph buildBinaryExpression(HLBinaryExpression binaryExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildBinaryExpression(HLBinaryExpression binaryExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Phil
-  public static LLControlFlowGraph buildUnaryExpression(HLUnaryExpression unaryExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildUnaryExpression(HLUnaryExpression unaryExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Noah
-  public static LLControlFlowGraph buildLoadExpression(HLLoadExpression loadScalarExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildLoadExpression(HLLoadExpression loadScalarExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Robert
-  public static LLControlFlowGraph buildLoadScalarExpression(HLLoadScalarExpression loadScalarExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildLoadScalarExpression(HLLoadScalarExpression loadScalarExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildLoadArrayExpression(HLLoadArrayExpression loadArrayExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildLoadArrayExpression(HLLoadArrayExpression loadArrayExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildCallExpression(HLCallExpression callExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildCallExpression(HLCallExpression callExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildInternalCallExpression(HLInternalCallExpression internalCallExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildInternalCallExpression(HLInternalCallExpression internalCallExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildExternalCallExpression(HLExternalCallExpression externalCallExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildExternalCallExpression(HLExternalCallExpression externalCallExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildLengthExpression(HLLengthExpression lengthExpression, LLDeclaration result) {
+  public static LLControlFlowGraph buildLengthExpression(HLLengthExpression lengthExpression, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
   // TODO: Phil
-  public static LLControlFlowGraph buildIntegerLiteral(HLIntegerLiteral integerLiteral, LLDeclaration result) {
+  public static LLControlFlowGraph buildIntegerLiteral(HLIntegerLiteral integerLiteral, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
-  public static LLControlFlowGraph buildStringLiteral(HLStringLiteral stringLiteral, LLDeclaration result) {
+  public static LLControlFlowGraph buildStringLiteral(HLStringLiteral stringLiteral, LLMethodDeclaration methodDeclaration, LLDeclaration result) {
     throw new RuntimeException("not implemented");
   }
 
