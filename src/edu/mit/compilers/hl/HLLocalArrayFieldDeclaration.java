34665cec81a54e10fb0f673cc83d884a28c5abae
noahmpauls <noahmpauls@gmail.com>
2021-04-01 16:36:54 -0600

    updated HLLocalArrayFieldDecl length spec

--- src/edu/mit/compilers/hl/HLLocalArrayFieldDeclaration.java
+++ src/edu/mit/compilers/hl/HLLocalArrayFieldDeclaration.java
@@ -4,31 +4,45 @@ import java.util.Optional;
 
 import edu.mit.compilers.ll.*;
 
+// TODO: Noah (debugString)
 public class HLLocalArrayFieldDeclaration implements HLArrayFieldDeclaration {
 
   // NOTE: may be changing with hoisting
   private final int index;
-  private final int length;
+  private final HLIntegerLiteral length;
   private Optional<LLLocalArrayFieldDeclaration> ll;
 
   public HLLocalArrayFieldDeclaration(
     final int index,
-    final int length)
+    final HLIntegerLiteral length)
   {
-    throw new RuntimeException("not implemented");
+    this.index = index;
+    this.length = length;
+    this.ll = Optional.empty();
+  }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public HLIntegerLiteral getLength() {
+    return length;
   }
 
   public void setLL(LLLocalArrayFieldDeclaration ll) {
-    throw new RuntimeException("not implemented");
+    if (this.ll.isPresent()) {
+      throw new RuntimeException("ll has already been set");
+    } else {
+      this.ll = Optional.of(ll);
+    }
   }
 
   @Override
   public LLLocalArrayFieldDeclaration getLL() {
-    throw new RuntimeException("not implemented");
-  }
-
-  public int getLength() {
-    return this.length;
+    if (ll.isPresent()) {
+      return ll.get();
+    }
+    throw new RuntimeException("ll is empty");
   }
 
   @Override
