2c3d813908c9b2ecb04436429220476d36c4bb32
noahmpauls <noahmpauls@gmail.com>
2021-04-05 16:40:10 -0600

    java 15 submission attempt 1

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -ea --enable-preview -jar "$(git rev-parse --show-toplevel)/build/jar/Compiler.jar" "$@"
+java15 -ea --enable-preview -jar "$(git rev-parse --show-toplevel)/build/jar/Compiler.jar" "$@"
