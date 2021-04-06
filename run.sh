691d8231dda3e440c6c22f17eb997f323509abea
PhilMurzynowski <filipjasper@gmail.com>
2021-04-05 22:07:50 -0400

    updated build.sh and run.sh to work on server

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java15 -ea --enable-preview -jar "$(git rev-parse --show-toplevel)/build/jar/Compiler.jar" "$@"
+#java15 -ea --enable-preview -jar "$(git rev-parse --show-toplevel)/build/jar/Compiler.jar" "$@"
+"${JAVA_HOME_15}/bin/java" -ea --enable-preview -jar "$(git rev-parse --show-toplevel)/build/jar/Compiler.jar" "$@"
+
