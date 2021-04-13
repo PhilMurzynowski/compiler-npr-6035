48366269f1e6391e7fb24b7c4edda1306ff26694
PhilMurzynowski <filipjasper@gmail.com>
2021-04-13 18:10:32 -0400

    quick save, will resume CSE later, noticed DCE does not work on nested blocks

--- src/edu/mit/compilers/opt/CSETable.java
+++ src/edu/mit/compilers/opt/CSETable.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 public class CSETable {
 
+  private final LLMethodDeclaration methodDeclaration;
   private Map<LLDeclaration, String> mapVarToVal = new HashMap<>();
   private Map<String, String> mapExprToVal = new HashMap<>();
   private Map<String, LLDeclaration> mapExprToTmp = new HashMap<>();
 
   private int valCount;
-  private int tmpCount;
 
-  public CSETable() {
-
+  public CSETable(LLMethodDeclaration methodDeclaration) {
+    this.methodDeclaration = methodDeclaration;
   }
 
   // Adds var to value mapping if does not exist and return value
@@ -39,6 +39,21 @@ public class CSETable {
       this.valCount++;
       return val;
     }
+  }
+
+  // TODO
+  public LLDeclaration exprToTmp(String expr) {
+    throw new RuntimeException("not implemented");
+
+    /*
+    if (mapExprToTmp.containsKey(expr)) {
+      return mapExprToTmp.get(expr);
+    } else {
+
+      mapExprToTmp.put(expr, tmp);
+      return tmp;
+    }
+    */
   }
 
 }
