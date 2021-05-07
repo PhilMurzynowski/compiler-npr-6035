182bdfd1679013c5bafd35dfb5b9b42c421119ba
noahmpauls <noahmpauls@gmail.com>
2021-05-06 20:51:36 -0600

    added the best testing script you will ever see in your whole life

--- assemble.sh
+++ assemble.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
-# usage: ./assemble.sh <.dcf file path> <optimizations>
+# Usage:
+# ./assemble.sh  file.dcf  --opt=opts
 
-# example for tests/program.dcf:
-#   $ ./assemble.sh tests/program all
+IFS='.';
+FILE_ARRAY=($1);
+unset IFS;
 
+EXT=${FILE_ARRAY[-1]};
+
+if [[ "$EXT" != "dcf" ]]
+then
+  printf "%s is not a .dcf file\n" "$1";
+  exit 1;
+fi
+
+INDEX=${#1};
+FULL_FILE=${1:0:$INDEX-4};
+
+printf "\nDecaf file with path %s\n" "$FULL_FILE";
+
+printf "\nCompiler Output:\n";
+printf "%s\n" "--------------------------------------------------";
 # generate assembly
-./run.sh --target=assembly --opt=$2 $1.dcf -o $1.s
+./run.sh --target=assembly "$2" "$FULL_FILE".dcf -o "$FULL_FILE".s;
+printf "%s\n" "--------------------------------------------------";
+
 # generate executable
-gcc -no-pie -O0 $1.s -o $1.out
-# run executable
+gcc -no-pie -O0 "$FULL_FILE".s -o "$FULL_FILE".out;
+
 printf "\nExecutable Output:\n";
-printf "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-./$1.out
-printf "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+printf "%s\n" "--------------------------------------------------";
+# run executable
+./"$FULL_FILE".out;
+printf "%s\n" "--------------------------------------------------";
